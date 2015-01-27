3f2329ecfe8094c2dec056e654fa04efeb9f2691
Andrew Haith <andyhaith@gmail.com>
2015-01-27 11:38:40 +0000

    Support for paging of results. #4

--- embed.js
+++ embed.js
@@ -1,8 +1,17 @@
-var buildTable = function (data, selector) {
+var buildTable = function (data, element) {
     //heading
-    var div_element = $(selector);
-    var table = $('<table></table>');
-    div_element.append(table);
+//    var div_element = $(selector);
+    element.empty();
+    //replace the contents of the existing table if it's there. Else create it.
+    var table = $('table', element);
+    if (!table.length) {
+        table = $('<table></table>');
+        element.append(table);
+    }
+    else {
+        table.empty();
+    }
+  
     table.append($('<tr><th>Date</th><th>Speakers</th><th>Title</th><th>Venue</th></tr>'));
     for (var i=0; i<data._embedded.talks.length; i++)
     {
@@ -16,8 +25,25 @@ var buildTable = function (data, selector) {
                         + '</td><td>' + location_name
                         + '</td></tr>'));
     }
+    if(data._links.prev) {
+        var prevLink = $('<a href="#">Prev</a>')
+        element.append(prevLink);
+        prevLink.on('click', function(ev) {
+            var element = $(ev.target).parent();
+            queryURL(data._links.prev.href, buildTable, element);
+        });
+    }
+    if(data._links.next) {
+        var nextLink = $('<a href="#">Next</a>');
+        element.append(nextLink);
+        nextLink.on('click', function(ev) {
+            var element = $(ev.target).parent();
+            queryURL(data._links.next.href, buildTable, element);
+        });
+    }
 }
 
+    
 //given an array of speaker objects, return a comma separated list of their names
 function get_person_titles(persons) {
     var titles = persons.map( function (person) {
@@ -29,17 +55,17 @@ function get_person_titles(persons) {
     return titles.join(', ');
 }
 
-var buildList = function (data, selector) {
-    var div_element = $(selector);
+var buildList = function (data, element) {
+    element.empty();
     for(var i=0; i<data._embedded.talks.length; i++) {
         talk = data._embedded.talks[i];
         header = $('<h3>');
         header.html(talk.title);
-        div_element.append(header);
+        element.append(header);
         
         description = $('<p>');
         description.html(talk.description);
-        div_element.append(description);
+        element.append(description);
         
         bullets = $('<ul>');
         bullets.append($('<li>Speaker: ' + get_person_titles(talk._embedded.speakers) + '</li>'));
@@ -51,13 +77,29 @@ var buildList = function (data, selector) {
         bullets.append($('<li>' + talk.formatted_date + '</li>'));
         var location = talk._embedded.venue ? talk._embedded.venue.name : "TBA";
         bullets.append($('<li>' + location + '</li>'));
-        div_element.append(bullets);
+        element.append(bullets);
+    }
+    if(data._links.prev) {
+        var prevLink = $('<a href="#">Prev</a>')
+        element.append(prevLink);
+        prevLink.on('click', function(ev) {
+            var element = $(ev.target).parent();
+            queryURL(data._links.prev.href, buildList, element);
+        });
+    }
+    if(data._links.next) {
+        var nextLink = $('<a href="#">Next</a>');
+        element.append(nextLink);
+        nextLink.on('click', function(ev) {
+            var element = $(ev.target).parent();
+            queryURL(data._links.next.href, buildList, element);
+        });
     }
 }
 
 
 //Performs the query then calling the specified callback function with the results and the selector
-var queryTalks = function (params, callback, selector) {
+var queryTalks = function (params, callback, element) {
     var url_stem = "http://talks.local:8000/api/events/search?";
 //    var url_stem = "http://talks-dev.oucs.ox.ac.uk/api/events/search?";
     var terms = [];
@@ -71,22 +113,26 @@ var queryTalks = function (params, callback, selector) {
     terms = terms.concat( buildTermsFromArray(params.venues, 'venue') );
     terms = terms.concat( buildTermsFromArray(params.organising_departments, 'organising_department') );
     terms = terms.concat( buildTermsFromArray(params.topics, 'topic') );
-
+    
     var url = url_stem + terms.join('&');
 
+    queryURL(url, callback, element);
+
+}
+
+function queryURL(url, callback, element) {
     $.ajax({
         url: url,
         type: "get",
         dataType: "json",
         success: function (response) {
-            callback(response, selector);                    
+            callback(response, element);                    
         },
         failure: function (response) {
             console.log("AJAX call failed");
         }
     });
 }
-
 
 function buildTermsFromArray(array, key) {
     if(array) {
@@ -105,7 +151,7 @@ function showTable(params, selector) {
     if (!selector) {
         selector = 'body';
     }
-    queryTalks(params, buildTable, selector);
+    queryTalks(params, buildTable, $(selector));
 }
 
 //Perform the query, providing the buildTable callback and passing on the specified selector
@@ -114,5 +160,5 @@ function showList(params, selector) {
     if (!selector) {
         selector = 'body';
     }
-    queryTalks(params, buildList, selector);
+    queryTalks(params, buildList, $(selector));
 }
